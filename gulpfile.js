d3d3822b1bf89ed073e32cdb253cf3c31629b6f0
Bradley Grainger <bgrainger@gmail.com>
2015-09-14 23:03:05 -0700

    Add 'gulp watch'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var babel = require('gulp-babel');
+var batch = require('gulp-batch');
 var eslint = require('gulp-eslint');
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
 
 gulp.task('lint', function() {
 	return gulp.src('src/*.js')
@@ -16,4 +18,10 @@ gulp.task('default', ['lint'], function() {
 		.pipe(babel({ optional: ['runtime'] }))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['default'], function() {
+	watch('src/*.js', batch(function(events, done) {
+		gulp.start('default', done);
+	}));
 });

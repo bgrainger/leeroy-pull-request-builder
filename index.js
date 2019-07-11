a2e69926a7cd6ec872e53c0c72fb717a657dc679
Bradley Grainger <bgrainger@gmail.com>
2019-07-11 13:01:29 -0700

    Ignore Jenkins SSL errors.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 "use strict";
 
+// ignore errors for Jenkins' SSL certificate 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 var server = require('./dist/server');
 server.start(process.env.PORT, 'https://git.faithlife.dev/api/v3', process.env.GITHUB_TOKEN);
